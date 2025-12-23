c86b587c1c3e558a60a76240934cc939bcae31d0
Vamsi krishna <118727057+vamsikrishna2049@users.noreply.github.com>
2025-12-23 18:59:24 +0530

    provider

--- provider.tf
+++ provider.tf
@@ -3,12 +3,13 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.0" 
-    }  
-    random = {
-      source = "hashicorp/random"
-      version = ">= 3.0"
-    }           
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0"
+    }
   }
+}
+
+# Provider Block
+provider "azurerm" {
+  features {}
 }
